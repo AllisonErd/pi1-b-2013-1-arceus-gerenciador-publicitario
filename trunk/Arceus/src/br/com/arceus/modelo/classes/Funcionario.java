96f6d7982ff59d2a7c0a59fec46b70ef12b91587
Allison373@gmail.com <Allison373@gmail.com@f74258fa-bfeb-7420-aa93-0424736a03d5>
2013-04-05 14:52:34 +0000

    Criação da Classe Funcionario extendendo da Classe Pai Pessoa

---
+++ trunk/Arceus/src/br/com/arceus/modelo/classes/Funcionario.java
@@ -0,0 +1,22 @@
+package br.com.arceus.modelo.classes;
+import br.com.arceus.modelo.entidades.Pessoa;
+
+
+public class Funcionario extends Pessoa {
+	
+	Pessoa funcionario;
+	
+	public Funcionario(){
+		funcionario.getNome();
+		funcionario.getLogin();
+		funcionario.getSenha();
+		funcionario.getEndereço();
+		funcionario.getContato();
+		funcionario.getCargo();
+		funcionario.getSetor();
+		
+		
+	}
+	
+
+}

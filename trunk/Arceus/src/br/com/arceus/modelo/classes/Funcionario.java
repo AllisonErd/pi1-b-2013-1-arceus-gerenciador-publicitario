db3bf771a4f6831e9c7ace9a0281c91939ac90e7
Fillype.fafica@gmail.com <Fillype.fafica@gmail.com@f74258fa-bfeb-7420-aa93-0424736a03d5>
2013-04-11 08:27:58 +0000

    Criação das classes Funcionario e Cliente.

--- trunk/Arceus/src/br/com/arceus/modelo/classes/Funcionario.java
+++ trunk/Arceus/src/br/com/arceus/modelo/classes/Funcionario.java
@@ -3,9 +3,51 @@ import br.com.arceus.modelo.entidades.Pessoa;
 
 
 public class Funcionario extends Pessoa {
-	
 
 	
+	private String sexo, senha, tempoCasa;
+	private double salario;
+	
+	private Funcionario(String sexo, String senha, String tempoCasa, double salario){
+		
+		this.sexo = sexo;
+		this.senha = senha;
+		this.tempoCasa = tempoCasa;
+		this.salario = salario;
+		
+	}
+	
+	public String getSexo(){
+		return sexo;
+	}
+	
+	public void setSexo(String sexo){
+		this.sexo = sexo;
+	}
+	
+	public String getSenha(){
+		return senha;
+	}
+	
+	public void setSenha(String senha){
+		this.senha = senha;
+	}
+	
+	public String getTempoCasa(){
+		return tempoCasa;
+	}
+	
+	public void setTempoCasa(String tempoCasa){
+		this.tempoCasa = tempoCasa;
+	}
+	
+	public getSalario(){
+		return salario;
+	}
+	
+	public void setSalario(double salario){
+		this.salario = salario;
+	}
 		
 		
 	}
